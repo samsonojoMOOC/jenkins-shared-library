fbbf5adff839ba37a3a443b980d32579bbb69ee6
Samson <sojo@techlerator.com>
2025-05-10 13:30:14 +0100

    Initial commit

---
+++ vars/buildImage.groovy
@@ -0,0 +1,10 @@
+#!/user/bin/env groovy
+
+def call(){
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t samsonojo/demo-app:jma-2.0.0 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push samsonojo/demo-app/demo-app:jma-2.0.0'
+    }
+}
