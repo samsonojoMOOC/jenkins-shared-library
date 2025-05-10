0851a8031748121ab04c1fbaeee4d069e6195426
Samson Ojo <samsonojoMOOC@users.noreply.github.com>
2025-05-10 17:19:12 +0100

    corrected the error in the docker push groovy function

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -5,6 +5,6 @@ def call(){
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t samsonojo/demo-app:jma-2.0.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push samsonojo/demo-app/demo-app:jma-2.0.0'
+        sh 'docker push samsonojo/demo-app:jma-2.0.0'
     }
 }
