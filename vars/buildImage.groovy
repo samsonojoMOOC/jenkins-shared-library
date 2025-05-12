02a4bc427ff6573e912686a9c48df35a882b5568
Samson Ojo <samsonojoMOOC@users.noreply.github.com>
2025-05-12 08:25:51 +0100

    corrected error in groovy script

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,6 +1,6 @@
 #!/user/bin/env groovy
 
-def call(imageName){
+def call(String imageName){
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh "docker build -t $imageName ."
