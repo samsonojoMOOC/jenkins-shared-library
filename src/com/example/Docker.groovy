3a00ead80f048ebfb88ed0a7a444c5a08d778a7f
Samson <sojo@techlerator.com>
2025-05-18 08:40:46 +0100

    seperated the groovy files

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -10,10 +10,20 @@ class Docker implements Serializable {
 
     def buildDockerImage(String imageName){
         script.echo "building the docker image..."
+        script.sh "docker build -t $imageName ."
+    }
+
+    def dockerLogin(){
+        script.echo "docker login..."
         script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-            script.sh "docker build -t $imageName ."
             script.sh "echo '${script.$PASS}' | docker login -u '${script.USER}' --password-stdin"
-            script.sh "docker push $imageName"
+
         }
+    }
+
+    def dockerPush(String imageName){
+        script.echo "pushing docker image..."
+            script.sh "docker push $imageName"
+
     }
 }
